b05957d668f628a108b9e4034be9d9e2a9a13b2d
itsf4llofstars <irooted4hal@mailfence.com>
2023-04-12 15:15:00 -0500

    Add get response function

--- main.py
+++ main.py
@@ -3,7 +3,27 @@
 Main Python file to parse METAR (aviation weather data) from
 the internet
 """
+from urllib.request import urlopen
+
+
+def get_response(web_address: str):
+    """Gets a returned web-site response via the
+    urllib module
+
+    Args:
+        web_address (str): The web-site address for the response
+
+    Return:
+        response (str): Response from the polled web-site
+    """
+    try:
+        with urlopen(web_address) as resp:
+            response = resp.read()
+    except Exception as err:
+        print(f"{err}")
+    else:
+        return response
 
 
 if __name__ == "__main__":
-    pass
+    url_address = "https://www.python.org"
