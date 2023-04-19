d27d118b5eef1d1d36610609fc74411210ab3a5e
itsf4llofstars <irooted4hal@mailfence.com>
2023-04-19 14:47:18 -0500

    Auto line ALE and black

--- main.py
+++ main.py
@@ -97,8 +97,8 @@ if __name__ == "__main__":
     utf8_response = url_response.decode("utf-8")
 
     html_filename = os.path.expanduser(
-        os.path.join("~", "python", "metar_parser", "metar.html")
-    )
+            os.path.join("~", "python", "metar_parser", "metar.html")
+            )
 
     write_response(html_filename, utf8_response)
     metar_text = read_response_file(html_filename)
