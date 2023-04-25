f7ef0fea1c15b9af97e52ad094a2d63720b517c8
itsf4llofstars <irooted4hal@mailfence.com>
2023-04-25 00:33:24 -0500

    Add conditional to convert ZULU to LOCAL time

--- regex.py
+++ regex.py
@@ -26,6 +26,29 @@ raw_metar = get_metar(
     os.path.expanduser(os.path.join("~", "python", "metar_parser", "metar.txt"))
 )
 
+# KBOS 250354Z 03005KT 10SM BKN008 OVC065 09/07 A3007
+# 012345678901
 metar = strip_remarks(raw_metar)
 
 print(metar)
+
+station = metar[:4]
+print(station)
+
+day = int(metar[5:7])
+hour = int(metar[7:9])
+minute = int(metar[9:11])
+print(day, hour, minute)
+
+dst = 5
+local_hr = -1
+hour = 0
+while hour < 25:
+    if 0 <= hour < 5:
+        local_hr = hour + 24 - dst
+    elif hour == 5:
+        local_hr = hour - dst
+    elif 6 <= hour <= 23:
+        local_hr = hour - dst
+    print(hour, local_hr)
+    hour += 1
